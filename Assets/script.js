7c87b339a4fc901bc55565fadbdf4485ca873b17
Elziekio <anthony.deamer@hotmail.com>
2021-07-18 15:32:08 +1000

    5th updated files

--- Assets/script.js
+++ Assets/script.js
@@ -9,25 +9,23 @@ var APIKey ="5882b925eee672571dd3e84eb144c900";
 var CurrentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+inputValue+"&units=metric&appid=5882b925eee672571dd3e84eb144c900";
 
 //saving input value from search bar and storing in local storage.//
-SearchBtn.addEventListener("click", function(event){
+function saveInput(){
+ SearchBtn.addEventListener("click", function(event){
     event.preventDefault();
-    inputValue = inputValue.value;
+    inputValue = [inputValue.value];
     localStorage.setItem("City", inputValue);
     getSavedValue();
-});
-
+ });
+}
 
 //Getting the saved value from Local storage and apending it to the created li element for the recent search history.//
 function getSavedValue(){
     var getSavedItem = localStorage.getItem("City");
-    getSavedItem = [];
     var searchHistory = document.createElement("li");
     searchHistoryList.appendChild(searchHistory);
-    for (var i=0; i < getSavedItem.length; i++){
-        searchHistory.innerHTML= getSavedItem;
-    }
-    console.log(getSavedItem);
-  };
+    searchHistory.innerHTML= getSavedItem;
+    saveInput();
+};
 
 
 
