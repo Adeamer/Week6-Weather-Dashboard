df9706c293ec9f8eeca6856384c35fd8f05c1816
Elziekio <anthony.deamer@hotmail.com>
2021-07-18 12:33:51 +1000

    updating file commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,24 @@
 //Getting HTML elements to be manipulated//
 var APIKey ="5882b925eee672571dd3e84eb144c900";
-var SearchBtn = document.getElementById("#search-button");
-var inputValue = document.getElementById("#inputValue");
+var SearchBtn = document.getElementById("search-button");
+
+
 var requestURL = "https://api.openweathermap.org/data/2.5/weather?q="+inputValue+"&units=metric&appid=5882b925eee672571dd3e84eb144c900";
 
-console.log(requestURL);
-
-
-
-
-function getApi (requestURL){
-    fetch(requestURL)
-        .then(function (response) {
-            console.log(response)
-        })
+function Searchinput(){
+    var inputValue = document.getElementById("inputValue").value;
+    console.log(inputValue);
 };
+
+
+
+
+// function getApi (requestURL){
+//     fetch(requestURL)
+//         .then(function (response) {
+//             console.log(response)
+//         })
+// };
 // function performSearch () {
 //     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Brisbane&units=metric&appid=5882b925eee672571dd3e84eb144c900", function(data){
 //      console.log(data);
