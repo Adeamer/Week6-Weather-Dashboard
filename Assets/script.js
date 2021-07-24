17281a6c186257a0118b61be2aa1e4937cb89afa
Elziekio <anthony.deamer@hotmail.com>
2021-07-24 12:15:43 +1000

    updating files

--- Assets/script.js
+++ Assets/script.js
@@ -22,13 +22,14 @@ searchBtn.addEventListener("click", function(event){
     var savedCities= []
     savedCities.push(inputValue.value);
     localStorage.setItem("City", JSON.stringify(savedCities));
+    //Pulling API data for current weather.
     var APIKey ="5882b925eee672571dd3e84eb144c900";
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ inputValue.value +"&units=metric&appid="+ APIKey;
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
-    var uviUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&exclude=current,minutely,hourly&appid="+ APIKey;
-    console.log(uviUrl);
+    
+    //Pulling input value from local storage.
     JSON.parse(localStorage.getItem("City"));
+
+    //fetching current weather API and displaying data from input value.
     $.getJSON(requestUrl, function(data){
         
            var name= data.name;
@@ -36,6 +37,8 @@ searchBtn.addEventListener("click", function(event){
            var temp= Math.floor(data.main.temp);
            var icon="https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
            var humidity= data.main.humidity;
+           var lat = data.coord.lat;
+           var lon = data.coord.lon;
 
         
            $(".city-name").append(name);
@@ -43,12 +46,22 @@ searchBtn.addEventListener("click", function(event){
            $(".temp").append("Temp: ", temp, " Degrees");
            $(".wind").append("Wind Speed: ", wind, " Meters per second");
            $(".humidity").append("Humidity ", humidity, "%");
+
+           //Pulling UV index and 5 day forecast API
+           var uviUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&appid="+ APIKey;
+           
+           //Fetching and formating UV index and 5 days forecast into html.
+           $.getJSON(uviUrl, function(data){
+            console.log(data);
+            
+            var uvIndex = data.current.uvi;
+            console.log(uvIndex);
+            $("uv-index").append("UV Index: ", uvIndex);
+
+            });
        })
 
-    $.getJSON(uviUrl, function(data){
-        console.log(data);
-        
-    })
+  
     getSavedValue();
 });
 
